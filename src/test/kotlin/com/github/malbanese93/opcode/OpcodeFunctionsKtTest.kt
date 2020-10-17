51ebf4898cfa118554174b20b1e9533d70860ee5
Michele Albanese <michele.albanese93@gmail.com>
2020-10-17 21:02:45 +0200

    Use CPU.update() instead of function directly

--- src/test/kotlin/com/github/malbanese93/opcode/OpcodeFunctionsKtTest.kt
+++ src/test/kotlin/com/github/malbanese93/opcode/OpcodeFunctionsKtTest.kt
@@ -2,9 +2,10 @@ package com.github.malbanese93.opcode
 
 import com.github.malbanese93.chip8.*
 import com.github.malbanese93.chip8.Memory.Companion.FONT_SIZE_IN_BYTES
-import com.github.malbanese93.utils.START_PC
-import com.github.malbanese93.exceptions.ValueExceedingByteException
 import com.github.malbanese93.exceptions.ValueExceedingNibbleException
+import com.github.malbanese93.extensions.highByte
+import com.github.malbanese93.extensions.lowByte
+import com.github.malbanese93.utils.START_PC
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.BeforeEach
@@ -49,110 +50,112 @@ internal class OpcodeFunctionsKtTest {
 
     @Test
     fun jumpToNNN() { // 1NNN
-        val opcode = 0x1BEA
+        setNextInstruction(0x1BEA)
 
-        jumpToNNN(opcode, cpu)
+        cpu.update()
         assertEquals(0x0BEA, cpu.regs.PC)
     }
 
     @Test
     fun jumpToV0PlusNNN() { // BNNN
-        val opcode = 0xBBEA
+        setNextInstruction(0xBBEA)
         cpu.regs.V[0] = 2
 
-        jumpToV0PlusNNN(opcode, cpu)
+        cpu.update()
         assertEquals(0x0BEC, cpu.regs.PC)
     }
 
     @Test
     fun skipIfVxEqNN() {    // 3XNN
-        val opcode = 0x3ABA
+        setNextInstruction(0x3ABA)
         cpu.regs.V[0xA] = 0xBA
 
-        skipIfVxEqNN(opcode, cpu)
+        cpu.update()
         assertEquals(startPC + 4, cpu.regs.PC) // skip one instruction
     }
 
     @Test
     fun skipIfVxEqNNComparisonFails() {    // 3XNN
-        val opcode = 0x3ABA
+        setNextInstruction(0x3ABA)
         cpu.regs.V[0xA] = 0xAB
 
-        skipIfVxEqNN(opcode, cpu)
+        cpu.update()
         assertEquals(startPC + 2, cpu.regs.PC) // DO NOT skip one instruction
     }
 
     @Test
     fun skipIfVxNotEqNN() { // 4XNN
-        val opcode = 0x4CCD
+        setNextInstruction(0x4CCD)
         cpu.regs.V[0xC] = 0xBA
 
-        skipIfVxNotEqNN(opcode, cpu)
+        cpu.update()
         assertEquals(startPC + 4, cpu.regs.PC) // skip one instruction
     }
 
     @Test
     fun skipIfVxNotEqNNComparisonFails() { // 4XNN
-        val opcode = 0x4CCD
+        setNextInstruction(0x4CCD)
         cpu.regs.V[0xC] = 0xCD
 
-        skipIfVxNotEqNN(opcode, cpu)
+        cpu.update()
         assertEquals(startPC + 2, cpu.regs.PC) // DO NOT skip one instruction
     }
 
     @Test
     fun skipIfVxEqVy() {            // 5XY0
-        val opcode = 0x5AB0
+        setNextInstruction(0x5AB0)
         cpu.regs.V[0xA] = 0x1
         cpu.regs.V[0xB] = 0x1
 
-        skipIfVxEqVy(opcode, cpu)
+        cpu.update()
         assertEquals(startPC + 4, cpu.regs.PC) // skip one instruction
     }
 
     @Test
     fun skipIfVxEqVyComparisonFails() {            // 5XY0
-        val opcode = 0x5AB0
+        setNextInstruction(0x5AB0)
         cpu.regs.V[0xA] = 0x2
         cpu.regs.V[0xB] = 0x1
 
-        skipIfVxEqVy(opcode, cpu)
+        cpu.update()
         assertEquals(startPC + 2, cpu.regs.PC) // skip one instruction
     }
 
     @Test
     fun skipIfVxNotEqVy() {                         // 9XY0
-        val opcode = 0x9AB0
+        setNextInstruction(0x9AB0)
         cpu.regs.V[0xA] = 0x1
         cpu.regs.V[0xB] = 0x2
 
-        skipIfVxNotEqVy(opcode, cpu)
+        cpu.update()
         assertEquals(startPC + 4, cpu.regs.PC) // skip one instruction
     }
 
     @Test
     fun skipIfVxNotEqVyComparisonFails() {          // 9XY0
-        val opcode = 0x9AB0
+        setNextInstruction(0x9AB0)
         cpu.regs.V[0xA] = 0x2
         cpu.regs.V[0xB] = 0x2
 
-        skipIfVxNotEqVy(opcode, cpu)
+        cpu.update()
         assertEquals(startPC + 2, cpu.regs.PC) // skip one instruction
     }
 
     @Test
     fun setVxToNN() {               // 6XNN
-        val opcode = 0x6ABC
-        setVxToNN(opcode, cpu)
+        setNextInstruction(0x6ABC)
+
+        cpu.update()
         assertEquals(0xBC, cpu.regs.V[0xA])
         assertEquals(startPC + 2, cpu.regs.PC)
     }
 
     @Test
     fun setVxToVxPlusNN() {         // 7XNN
-        val opcode = 0x7AEE
+        setNextInstruction(0x7AEE)
         cpu.regs.V[0xA] = 0x20
-        setVxToVxPlusNN(opcode, cpu)
+
+        cpu.update()
         assertEquals(0x0E, cpu.regs.V[0xA])
         assertEquals(0x00, cpu.regs.V[0xF]) // unchanged even with carry!
         assertEquals(startPC + 2, cpu.regs.PC)
@@ -160,49 +163,54 @@ internal class OpcodeFunctionsKtTest {
 
     @Test
     fun setVxToVy() {               // 8XY0
-        val opcode = 0x8AB0
+        setNextInstruction(0x8AB0)
         cpu.regs.V[0xB] = 0xCD
-        setVxToVy(opcode, cpu)
+
+        cpu.update()
         assertEquals(0xCD, cpu.regs.V[0xA])
         assertEquals(startPC + 2, cpu.regs.PC)
     }
 
     @Test
     fun setVxToVxOrVy() {           // 8XY1
-        val opcode = 0x8AB1
+        setNextInstruction(0x8AB1)
         cpu.regs.V[0xA] = 0b11001010
         cpu.regs.V[0xB] = 0b01011001
-        setVxToVxOrVy(opcode, cpu)
+
+        cpu.update()
         assertEquals(0b11011011, cpu.regs.V[0xA])
         assertEquals(startPC + 2, cpu.regs.PC)
     }
 
     @Test
     fun setVxToVxAndVy() {          // 8XY2
-        val opcode = 0x8AB2
+        setNextInstruction(0x8AB2)
         cpu.regs.V[0xA] = 0b11001010
         cpu.regs.V[0xB] = 0b01011001
-        setVxToVxAndVy(opcode, cpu)
+
+        cpu.update()
         assertEquals(0b01001000, cpu.regs.V[0xA])
         assertEquals(startPC + 2, cpu.regs.PC)
     }
 
     @Test
     fun setVxToVxXorVy() {          // 8XY3
-        val opcode = 0x8AB3
+        setNextInstruction(0x8AB3)
         cpu.regs.V[0xA] = 0b11001010
         cpu.regs.V[0xB] = 0b01011001
-        setVxToVxXorVy(opcode, cpu)
+
+        cpu.update()
         assertEquals(0b10010011, cpu.regs.V[0xA])
         assertEquals(startPC + 2, cpu.regs.PC)
     }
 
     @Test
     fun setVxToVxPlusVy() {         // 8XY4
-        val opcode = 0x8AB4
+        setNextInstruction(0x8AB4)
         cpu.regs.V[0xA] = 0x23
         cpu.regs.V[0xB] = 0x01
-        setVxToVxPlusVy(opcode, cpu)
+
+        cpu.update()
         assertEquals(0x24, cpu.regs.V[0xA])
         assertEquals(0x00, cpu.regs.V[0xF])
         assertEquals(startPC + 2, cpu.regs.PC)
@@ -210,10 +218,11 @@ internal class OpcodeFunctionsKtTest {
 
     @Test
     fun setVxToVxPlusVyWithCarry() {         // 8XY4
-        val opcode = 0x8AB4
+        setNextInstruction(0x8AB4)
         cpu.regs.V[0xA] = 0xFE
         cpu.regs.V[0xB] = 0x03
-        setVxToVxPlusVy(opcode, cpu)
+
+        cpu.update()
         assertEquals(0x01, cpu.regs.V[0xA])
         assertEquals(0x01, cpu.regs.V[0xF])
         assertEquals(startPC + 2, cpu.regs.PC)
@@ -221,10 +230,11 @@ internal class OpcodeFunctionsKtTest {
 
     @Test
     fun setVxToVxMinusVy() {                // 8XY5
-        val opcode = 0x8AB5
+        setNextInstruction(0x8AB5)
         cpu.regs.V[0xA] = 0x23
         cpu.regs.V[0xB] = 0x01
-        setVxToVxMinusVy(opcode, cpu)
+
+        cpu.update()
         assertEquals(0x22, cpu.regs.V[0xA])
         assertEquals(0x00, cpu.regs.V[0xF])
         assertEquals(startPC + 2, cpu.regs.PC)
@@ -232,10 +242,11 @@ internal class OpcodeFunctionsKtTest {
 
     @Test
     fun setVxToVxMinusVyWithBorrow() {                // 8XY5
-        val opcode = 0x8AB5
+        setNextInstruction(0x8AB5)
         cpu.regs.V[0xA] = 0x23
         cpu.regs.V[0xB] = 0x25
-        setVxToVxMinusVy(opcode, cpu)
+
+        cpu.update()
         assertEquals(0xFE, cpu.regs.V[0xA])
         assertEquals(0x01, cpu.regs.V[0xF])
         assertEquals(startPC + 2, cpu.regs.PC)
@@ -243,9 +254,10 @@ internal class OpcodeFunctionsKtTest {
 
     @Test
     fun setVxToVxShr1() {           // 8XY6
-        val opcode = 0x8AB6
+        setNextInstruction(0x8AB6)
         cpu.regs.V[0xA] = 0b1001_1001
-        setVxToVxShr1(opcode, cpu)
+
+        cpu.update()
         assertEquals(0b0100_1100, cpu.regs.V[0xA])
         assertEquals(0x01, cpu.regs.V[0xF])
         assertEquals(startPC + 2, cpu.regs.PC)
@@ -253,9 +265,10 @@ internal class OpcodeFunctionsKtTest {
 
     @Test
     fun setVxToVxShr1WithLsb0() {           // 8XY6
-        val opcode = 0x8AB6
+        setNextInstruction(0x8AB6)
         cpu.regs.V[0xA] = 0b1001_1000
-        setVxToVxShr1(opcode, cpu)
+
+        cpu.update()
         assertEquals(0b0100_1100, cpu.regs.V[0xA])
         assertEquals(0x00, cpu.regs.V[0xF])
         assertEquals(startPC + 2, cpu.regs.PC)
@@ -263,10 +276,11 @@ internal class OpcodeFunctionsKtTest {
 
     @Test
     fun setVxToVyMinusVx() {                // 8XY7
-        val opcode = 0x8AB7
+        setNextInstruction(0x8AB7)
         cpu.regs.V[0xA] = 0x23
         cpu.regs.V[0xB] = 0x25
-        setVxToVyMinusVx(opcode, cpu)
+
+        cpu.update()
         assertEquals(0x02, cpu.regs.V[0xA])
         assertEquals(0x00, cpu.regs.V[0xF])
         assertEquals(startPC + 2, cpu.regs.PC)
@@ -274,10 +288,11 @@ internal class OpcodeFunctionsKtTest {
 
     @Test
     fun setVxToVyMinusVxWithBorrow() {             // 8XY7
-        val opcode = 0x8AB7
+        setNextInstruction(0x8AB7)
         cpu.regs.V[0xA] = 0x03
         cpu.regs.V[0xB] = 0x02
-        setVxToVyMinusVx(opcode, cpu)
+
+        cpu.update()
         assertEquals(0xFF, cpu.regs.V[0xA])
         assertEquals(0x01, cpu.regs.V[0xF])
         assertEquals(startPC + 2, cpu.regs.PC)
@@ -285,9 +300,10 @@ internal class OpcodeFunctionsKtTest {
 
     @Test
     fun setVxToVxShl1() {           // 8XYE
-        val opcode = 0x8ABE
+        setNextInstruction(0x8ABE)
         cpu.regs.V[0xA] = 0b1001_1001
-        setVxToVxShl1(opcode, cpu)
+
+        cpu.update()
         assertEquals(0b0011_0010, cpu.regs.V[0xA])
         assertEquals(0x01, cpu.regs.V[0xF])
         assertEquals(startPC + 2, cpu.regs.PC)
@@ -295,9 +311,10 @@ internal class OpcodeFunctionsKtTest {
 
     @Test
     fun setVxToVxShl1WithMsb0() {           // 8XYE
-        val opcode = 0x8ABE
+        setNextInstruction(0x8ABE)
         cpu.regs.V[0xA] = 0b0001_1001
-        setVxToVxShl1(opcode, cpu)
+
+        cpu.update()
         assertEquals(0b0011_0010, cpu.regs.V[0xA])
         assertEquals(0x00, cpu.regs.V[0xF])
         assertEquals(startPC + 2, cpu.regs.PC)
@@ -305,72 +322,103 @@ internal class OpcodeFunctionsKtTest {
 
     @Test
     fun setVxToDelayTimer() {           // FX07
-        val opcode = 0xFA07
+        setNextInstruction(0xFA07)
         cpu.regs.DT = 0x23
-        setVxToDelayTimer(opcode, cpu)
+
+        cpu.update()
+
         assertEquals(0x23, cpu.regs.V[0xA])
         assertEquals(startPC + 2, cpu.regs.PC)
     }
 
     @Test
     fun setDelayTimerToVx() {           // FX15
-        val opcode = 0xFA15
+        setNextInstruction(0xFA15)
         cpu.regs.V[0xA] = 0x23
-        setDelayTimerToVx(opcode, cpu)
+
+        cpu.update()
         assertEquals(0x23, cpu.regs.DT)
         assertEquals(startPC + 2, cpu.regs.PC)
     }
 
     @Test
     fun setSoundTimerToVx() {           // FX18
-        val opcode = 0xFA18
+        setNextInstruction(0xFA18)
         cpu.regs.V[0xA] = 0x23
-        setSoundTimerToVx(opcode, cpu)
+
+        cpu.update()
         assertEquals(0x23, cpu.regs.ST)
         assertEquals(startPC + 2, cpu.regs.PC)
     }
 
     @Test
     fun setIToNNN() {               // ANNN
-        val opcode = 0xACBA
-        setIToNNN(opcode, cpu)
+        setNextInstruction(0xACBA)
+
+        cpu.update()
         assertEquals(0xCBA, cpu.regs.I)
         assertEquals(startPC + 2, cpu.regs.PC)
     }
 
     @Test
     fun setIToIPlusVx() {           // FX1E
-        val opcode = 0xFA1E
+        setNextInstruction(0xFA1E)
         cpu.regs.V[0xA] = 0x0A
         cpu.regs.I = 0xFFFE
-        setIToIPlusVx(opcode, cpu)
+
+        cpu.update()
         assertEquals(0x8, cpu.regs.I)
-        assertEquals(startPC + 2, cpu.regs.PC)
         assertEquals(0x00, cpu.regs.V[0xF]) // unchanged even with carry!
+        assertEquals(startPC + 2, cpu.regs.PC)
     }
 
     @Test
     fun setIToSpriteLocation() {        // FX29
-        val opcode = 0xFA29
+        setNextInstruction(0xFA29)
         cpu.regs.V[0xA] = 0xB
-        setIToSpriteLocation(opcode, cpu)
+
+        cpu.update()
         assertEquals(FONT_SIZE_IN_BYTES * 0xB, cpu.regs.I)
         assertEquals(startPC + 2, cpu.regs.PC)
     }
 
     @Test
     fun setIToSpriteLocationFailsWithOutOfRangeValue() {        // FX29
-        val opcode = 0xFA29
+        setNextInstruction(0xFA29)
         cpu.regs.V[0xA] = 0x10
-        Assertions.assertThrows(ValueExceedingNibbleException::class.java) { setIToSpriteLocation(opcode, cpu) }
+
+        Assertions.assertThrows(ValueExceedingNibbleException::class.java) { cpu.update() }
     }
 
     @Test
-    fun bcd() {
+    fun bcd() {             // FX33
+        setNextInstruction(0xFA33)
+        cpu.regs.V[0xA] = 234
+
+        cpu.update()
+        assertEquals(2, cpu.memory[cpu.regs.I])
+        assertEquals(3, cpu.memory[cpu.regs.I+1])
+        assertEquals(4, cpu.memory[cpu.regs.I+2])
+        assertEquals(startPC + 2, cpu.regs.PC)
     }
 
     @Test
-    fun dumpVxRegisters() {
+    fun dumpVxRegisters() {         // FX55
+        setNextInstruction(0xF155)
+
+        cpu.regs.I = 0x0420
+        cpu.regs.V[0x0] = 0x1F
+        cpu.regs.V[0x1] = 0xAB
+
+        cpu.update()
+        assertEquals(0x1F, cpu.memory[cpu.regs.I])
+        assertEquals(0xAB, cpu.memory[cpu.regs.I+1])
+
+        for (idx in 0x2..0xF)
+            assertEquals(0x00, cpu.memory[cpu.regs.I+idx])
+
+        assertEquals(0x0420, cpu.regs.I)
+        assertEquals(startPC + 2, cpu.regs.PC)
     }
 
     @Test
@@ -391,5 +439,10 @@ internal class OpcodeFunctionsKtTest {
 
     @Test
     fun clearDisplay() {
+    }
+
+    private fun setNextInstruction(opcode: Int) {
+        cpu.memory[cpu.regs.PC] = opcode.highByte
+        cpu.memory[cpu.regs.PC+1] = opcode.lowByte
     }
 }
